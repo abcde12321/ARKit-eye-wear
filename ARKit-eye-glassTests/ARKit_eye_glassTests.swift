f6283c997b8617d99c7255cff05749ea8843fc0e
Fan Yang <cyangfan@gmail.com>
2020-11-19 22:18:53 +0100

    Initial Commit

---
+++ ARKit-eye-glassTests/ARKit_eye_glassTests.swift
@@ -0,0 +1,33 @@
+//
+//  ARKit_eye_glassTests.swift
+//  ARKit-eye-glassTests
+//
+//  Created by Fan Yang on 11/19/20.
+//
+
+import XCTest
+@testable import ARKit_eye_glass
+
+class ARKit_eye_glassTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
