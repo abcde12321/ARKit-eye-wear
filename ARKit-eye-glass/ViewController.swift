ae14aeed2e46530fce5257d609c2318bda31a489
Fan Yang <cyangfan@gmail.com>
2020-11-20 10:09:38 +0100

    first commit

--- ARKit-eye-glass/ViewController.swift
+++ ARKit-eye-glass/ViewController.swift
@@ -1,74 +1,133 @@
-//
-//  ViewController.swift
-//  ARKit-eye-glass
-//
-//  Created by Fan Yang on 11/19/20.
-//
+/*
+See LICENSE folder for this sample’s licensing information.
 
-import UIKit
-import SceneKit
+Abstract:
+Main view controller for the AR experience.
+*/
+
 import ARKit
+import SceneKit
+import UIKit
 
-class ViewController: UIViewController, ARSCNViewDelegate {
+class ViewController: UIViewController, ARSessionDelegate {
+    
+    // MARK: Outlets
 
     @IBOutlet var sceneView: ARSCNView!
+
+    // MARK: Properties
+
+    var contentControllers: [VirtualContentType: VirtualContentController] = [:]
     
+    var selectedVirtualContent: VirtualContentType! {
+        didSet {
+            guard oldValue != nil, oldValue != selectedVirtualContent
+                else { return }
+            
+            // Remove existing content when switching types.
+            contentControllers[oldValue]?.contentNode?.removeFromParentNode()
+            
+            // If there's an anchor already (switching content), get the content controller to place initial content.
+            // Otherwise, the content controller will place it in `renderer(_:didAdd:for:)`.
+            if let anchor = currentFaceAnchor, let node = sceneView.node(for: anchor),
+                let newContent = selectedContentController.renderer(sceneView, nodeFor: anchor) {
+                node.addChildNode(newContent)
+            }
+        }
+    }
+    var selectedContentController: VirtualContentController {
+        return FaceOcclusionOverlay()
+    }
+    
+    var currentFaceAnchor: ARFaceAnchor?
+    
+    // MARK: - View Controller Life Cycle
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // Set the view's delegate
         sceneView.delegate = self
+        sceneView.session.delegate = self
+        sceneView.automaticallyUpdatesLighting = true
         
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        
-        // Create a new scene
-        let scene = SCNScene(named: "art.scnassets/ship.scn")!
-        
-        // Set the scene to the view
-        sceneView.scene = scene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        
-        // Pause the view's session
-        sceneView.session.pause()
+        // Set the initial face content.
+        //tabBar.selectedItem = tabBar.items!.first!
+        //selectedVirtualContent = VirtualContentType(rawValue: tabBar.selectedItem!.tag)
     }
 
-    // MARK: - ARSCNViewDelegate
-    
-/*
-    // Override to create and configure nodes for anchors added to the view's session.
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-     
-        return node
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        // AR experiences typically involve moving the device without
+        // touch input for some time, so prevent auto screen dimming.
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        // "Reset" to run the AR session for the first time.
+        resetTracking()
     }
-*/
-    
+
+    // MARK: - ARSessionDelegate
+
     func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+        guard error is ARError else { return }
         
+        let errorWithInfo = error as NSError
+        let messages = [
+            errorWithInfo.localizedDescription,
+            errorWithInfo.localizedFailureReason,
+            errorWithInfo.localizedRecoverySuggestion
+        ]
+        let errorMessage = messages.compactMap({ $0 }).joined(separator: "\n")
+        
+        DispatchQueue.main.async {
+            self.displayErrorMessage(title: "The AR session failed.", message: errorMessage)
+        }
     }
     
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-        
+    /// - Tag: ARFaceTrackingSetup
+    func resetTracking() {
+        guard ARFaceTrackingConfiguration.isSupported else { return }
+        let configuration = ARFaceTrackingConfiguration()
+        configuration.isLightEstimationEnabled = true
+        sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
     
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
+    // MARK: - Error handling
+    
+    func displayErrorMessage(title: String, message: String) {
+        // Present an alert informing about the error that has occurred.
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let restartAction = UIAlertAction(title: "Restart Session", style: .default) { _ in
+            alertController.dismiss(animated: true, completion: nil)
+            self.resetTracking()
+        }
+        alertController.addAction(restartAction)
+        present(alertController, animated: true, completion: nil)
     }
 }
+
+
+extension ViewController: ARSCNViewDelegate {
+        
+    func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
+        guard let faceAnchor = anchor as? ARFaceAnchor else { return }
+        currentFaceAnchor = faceAnchor
+        
+        // If this is the first time with this anchor, get the controller to create content.
+        // Otherwise (switching content), will change content when setting `selectedVirtualContent`.
+        if node.childNodes.isEmpty, let contentNode = selectedContentController.renderer(renderer, nodeFor: faceAnchor) {
+            node.addChildNode(contentNode)
+        }
+    }
+    
+    /// - Tag: ARFaceGeometryUpdate
+    func renderer(_ renderer: SCNSceneRenderer, didUpdate node: SCNNode, for anchor: ARAnchor) {
+        guard anchor == currentFaceAnchor,
+            let contentNode = selectedContentController.contentNode,
+            contentNode.parent == node
+            else { return }
+        
+        selectedContentController.renderer(renderer, didUpdate: contentNode, for: anchor)
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,9 @@
+#  ARkit eye wear
+
+
+Experimental project to put glasses on in AR. Using ARkit front camera face tracking.
+
+
+todo: use a real glasses object file intead of simple geometry. 
+
+![Alt Text](./VideoToGif.gif)
